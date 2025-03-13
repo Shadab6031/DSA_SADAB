9220dbe678a15c86ee05f57ba3c8e8e9c2a17477
Shadab6031 <khanshadab4046@gmail.com>
2025-03-14 00:07:11 +0530

    FUNCTION IN ARRAY(basic)

---
+++ ArrayFunction.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class ArrayFunction 
+{
+    public static void Update(int[]marks)
+    {
+      for(int i=0;i<marks.length;i++)
+      {
+        marks[i]=marks[i]+1; 
+      }
+    }
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("enter size of array - ");
+        int n=s.nextInt();
+        int[] marks =new int[n];
+        System.out.println("enter marks - ");
+        for(int i=0;i<marks.length;i++)
+        {
+            marks[i]=s.nextInt();
+        }
+        Update(marks);
+        System.out.println("updated marks is");
+    for(int i =0; i<marks.length;i++)
+    {
+      System.out.print(+marks[i]+" ");
+    }
+    System.out.println();
+    }
+}
