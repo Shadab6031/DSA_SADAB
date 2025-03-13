7a97ca79d6674a8d499d807a66dc88b27d648035
Shadab6031 <khanshadab4046@gmail.com>
2025-03-14 00:53:54 +0530

    LINEAR SEARCH IN ARRAY

---
+++ LinearSearchArray.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class LinearSearchArray 
+{
+    public static int linear(int[] arr,int key)
+    {
+      
+      for(int i=0;i<arr.length;i++)
+      {
+         if(key==arr[i])
+         {
+            return i;
+         }
+      }
+      return -1;
+    }
+    public static void main(String[] args) 
+    {
+        Scanner s = new Scanner(System.in);
+        System.out.print("enter the size of array -");
+        int size=s.nextInt();
+        int[]arr=new int[size];
+        System.out.println("enter array elements ");
+        for(int i =0;i<arr.length;i++)
+        {
+            arr[i]=s.nextInt();
+        }
+        System.out.println(Arrays.toString(arr));
+        System.out.println("enter an element to search ");
+       int key =s.nextInt();
+        int index = linear(arr,key);
+        if(index==-1)
+        {
+          System.out.println("element not found");
+        }
+        else
+        {
+            System.out.println(key+" found at index "+index);
+        }
+    }
+}
