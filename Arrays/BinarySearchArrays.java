30bbd41a4acde7330a5c4ce834740d625ebd742a
Shadab6031 <khanshadab4046@gmail.com>
2025-03-15 12:20:56 +0530

    BINARY SEARCH

---
+++ Arrays/BinarySearchArrays.java
@@ -0,0 +1,40 @@
+public class BinarySearchArrays 
+{
+    public static int binary(int key , int[]arr)
+    {
+        int left =0;
+        int right =arr.length-1;
+        while(left<=right)
+        {
+            int mid =(left + right)/2;
+            if(arr[mid]==key)
+            {
+                return mid;
+            }
+            else if(arr[mid]<key)
+            {
+                left =mid+1;
+            }
+            else
+            {
+                right=mid -1;
+            }
+        }
+        return -1;
+    }
+    public static void main(String[] args) 
+    {
+        int[] arr = new int[]{1,3,4,5,20};
+        int key =9;
+          int index=binary(key ,arr);
+            if(index ==-1)
+            {
+                System.out.println("element not found");
+            }
+            else 
+        {
+            System.out.println("element found at index "+index) ;
+        }
+
+     }
+}
